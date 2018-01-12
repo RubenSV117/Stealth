5e00ffe373932d5146532a526ef8f25326e5ec98
Ruben Sanchez <SanchezVRuben@gmail.com>
2018-01-12 00:19:42 -0800

    Triple Shot, Particle Effect on Bullet Bounce and Muzzle Flash

---
+++ FPSGame/Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSObjectiveActor.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "FPSCharacter.h"
+
+// Sets default values
+AFPSObjectiveActor::AFPSObjectiveActor()
+{
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision); 
+	MeshComp->SetPhysicsAngularVelocity(FVector(0, 0, 360));
+	RootComponent = MeshComp;
+
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	SphereComp->bGenerateOverlapEvents = true;
+	SphereComp->SetupAttachment(MeshComp);
+}
+
+// Called when the game starts or when spawned
+void AFPSObjectiveActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AFPSObjectiveActor::PlayPickUpEffect()
+{
+	UGameplayStatics::SpawnEmitterAtLocation(this, PickUpEffect, GetActorLocation());
+}
+
+void AFPSObjectiveActor::NotifyActorBeginOverlap(AActor * OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	PlayPickUpEffect();
+
+	AFPSCharacter* Player = Cast<AFPSCharacter>(OtherActor);
+
+	if (Player)
+	{
+		Player->bIsCarryingObjective = true;
+		PlayPickUpEffect();
+		Destroy();
+	}
+}
+
