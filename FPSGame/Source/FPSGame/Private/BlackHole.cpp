b61a38ed863d79de82841f8903022cc0881b95f6
Ruben Sanchez <SanchezVRuben@gmail.com>
2018-03-17 00:36:23 -0700

    BlackHoleBP

--- FPSGame/Source/FPSGame/Private/BlackHole.cpp
+++ FPSGame/Source/FPSGame/Private/BlackHole.cpp
@@ -1,14 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BlackHole.h"
-#include "Components/SphereComponent.h"
-#include "Kismet/GameplayStatics.h"
-#include "Components/PrimitiveComponent.h"
-#include "Components/SphereComponent.h"
-#include "Components/StaticMeshComponent.h"
-#include "GameFramework/Actor.h"
 
-#define OUT
 
 // Sets default values
 ABlackHole::ABlackHole()
@@ -16,41 +9,31 @@ ABlackHole::ABlackHole()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
-	MeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-	RootComponent = MeshComponent;
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	RootComponent = MeshComp;
 
 	InnerSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("InnerSphereComp"));
 	InnerSphereComponent->SetSphereRadius(100);
-	InnerSphereComponent->SetupAttachment(MeshComponent);
+    InnerSphereComponent->SetupAttachment(MeshComp);
 	InnerSphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ABlackHole::OverlapInnerSphere);
 
 	OuterSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("OuterSphereComp"));
 	OuterSphereComponent->SetSphereRadius(3000);
-	OuterSphereComponent->SetupAttachment(MeshComponent);
+	OuterSphereComponent->SetupAttachment(MeshComp);
 }
 
-void ABlackHole::Pull()
+// Called when the game starts or when spawned
+void ABlackHole::BeginPlay()
 {
-	TArray<AActor*> boxes;
-	OuterSphereComponent->GetOverlappingActors(OUT boxes);
-
-	for (AActor* box : boxes)
-	{
-		UPrimitiveComponent* mesh = Cast<UPrimitiveComponent>(box->GetRootComponent());
-
-		if (mesh)
-			mesh->AddRadialForce(GetOwner()->GetActorLocation(), OuterSphereComponent->GetScaledSphereRadius(), -1000000, ERadialImpulseFalloff::RIF_Linear);	
-	}
+	Super::BeginPlay();
+	
 }
 
-void ABlackHole::OverlapInnerSphere(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+void ABlackHole::OverlapInnerSphere(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
 	if (OtherActor)
-	{
-		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), Explosion, OtherActor->GetOwner()->GetActorLocation());
 		OtherActor->Destroy();
-	}
 }
 
 // Called every frame
@@ -58,6 +41,23 @@ void ABlackHole::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	Pull();
+	//Find all overlapping components that are simulating physics and pull them in
+	TArray<UPrimitiveComponent*> OverlappingComps;
+	OuterSphereComponent->GetOverlappingComponents(OUT OverlappingComps);
+
+	for (size_t i = 0; i < OverlappingComps.Num(); i++)
+	{
+		UPrimitiveComponent* overlappedComp = OverlappingComps[i];
+
+		if (overlappedComp && overlappedComp->IsSimulatingPhysics())
+		{
+			const float sphereRadius = OuterSphereComponent->GetScaledSphereRadius();
+			const float pullStrength = -2000;
+
+			overlappedComp->AddRadialForce(GetActorLocation(), sphereRadius, pullStrength, ERadialImpulseFalloff::RIF_Constant, true);
+		}
+	}
+
+
 }
 
